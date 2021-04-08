531919492d6b45026180271c14fbc55aeb908885
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-04-09 07:09:26 +0900

    step2: 変数のインライン化

--- apps/performances/src/app/statement.ts
+++ apps/performances/src/app/statement.ts
@@ -1,29 +1,34 @@
 import { Invoice, Performance, Play, Plays } from './types';
 
-const amountFor = (pref: Performance, play: Play) => {
-  let result = 0;
-
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000;
-      if (pref.audience > 30) {
-        result += 1000 * (pref.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (pref.audience > 20) {
-        result += 10000 + 500 * (pref.audience - 20);
-      }
-      result += 300 * pref.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-  return result;
-};
-
 export const statement = (invoice: Invoice, plays: Plays) => {
+
+  const amountFor = (pref: Performance, play: Play) => {
+    let result = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (pref.audience > 30) {
+          result += 1000 * (pref.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (pref.audience > 20) {
+          result += 10000 + 500 * (pref.audience - 20);
+        }
+        result += 300 * pref.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return result;
+  };
+
+  const playFor = (performance: Performance): Play => {
+    return plays[performance.playID]
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -34,16 +39,15 @@ export const statement = (invoice: Invoice, plays: Plays) => {
   }).format;
 
   for (const pref of invoice.performances) {
-    const play: Play = plays[pref.playID];
-    const thisAmount = amountFor(pref, play);
+    const thisAmount = amountFor(pref, playFor(pref));
 
     volumeCredits += Math.max(pref.audience - 30, 0);
 
-    if ('comedy' === play.type) {
+    if ('comedy' === playFor(pref).type) {
       volumeCredits += Math.floor(pref.audience / 5);
     }
 
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    result += ` ${playFor(pref).name}: ${format(thisAmount / 100)} (${
       pref.audience
     } seats) \n`;
     totalAmount += thisAmount;
